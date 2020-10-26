99fdfcc73c11b517cbafb1941c718c616588f8fd
Jody Albritton <jody@pop-os.localdomain>
2020-10-26 14:39:16 -0700

    rules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+
+
+import './App.scss';
+import AceEditor from "react-ace";
+
+import bonVoyage from './rules/bonVoyage.json'
+import brightenMyPath from './rules/brightenMyPath.json'
+
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-monokai";
+
+
+
+
+function onChange(newValue) {
+  console.log("change", newValue);
+}
+
+function loadFile(file) {
+  console.log(file)
+}
 
 function App() {
+  const [rule, setRule] = useState("{}");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        
+      <div className="container">
+      <h1 className="title">Rule Helper</h1>
+        <div className="columns is-mobile">
+          <div className="column is-one-third">
+            <div className="left-menu">
+              <div className="block">
+                <button className="button" onClick={() => setRule(JSON.stringify(bonVoyage, null, 4))}>BonVoyage</button>
+              </div>
+              <div className="block">
+                <button className="button" onClick={() => setRule(JSON.stringify(brightenMyPath, null, 4))}>Brighten My Path</button>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div id="example">
+              <AceEditor
+              mode="json"
+              theme="monokai"
+              onChange={onChange}
+              name="textarea"
+              value={rule}
+              editorProps={{ $blockScrolling: true }} 
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
+    
   );
 }
 
 export default App;
+
+
+
+
