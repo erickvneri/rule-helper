5f2d5e067479f159dd4a8f06c6a579d740b613f9
erickvneri <erickv@bluetrailsoft.com>
2020-10-30 17:45:37 -0600

    advanced - basic templates

--- src/templates/ruleTemplates.js
+++ src/templates/ruleTemplates.js
@@ -1,6 +1,9 @@
 // Basic templates
 import and from "./templates/and.json";
 import between from "./templates/between.json";
+import command from "./templates/command.json";
+import dropsBelow from "./templates/dropsBelow.json";
+import dropsToOrBelow from "./templates/dropsToOrBelow.json";
 import ifElse from "./templates/ifElse.json";
 import ifGreaterThan from "./templates/ifGreaterThan.json";
 import ifGreaterThanOrEquals from "./templates/ifGreaterThanOrEquals.json";
@@ -8,26 +11,37 @@ import ifNot from "./templates/ifNot.json";
 import lessThan from "./templates/lessThan.json";
 import lessThanOrEquals from "./templates/lessThanOrEquals.json";
 import or from "./templates/or.json";
+import risesAbove from "./templates/risesAbove.json";
+import risesToOrAbove from "./templates/risesToOrAbove.json";
+import setLocationMode from "./templates/setLocationMode.json";
 // Advanced templates
 import sleep from "./templates/sleep.json";
 import nestedIf from "./templates/nestedIf.json";
+import morningSchedule from "./templates/morningSchedule.json";
+
 
 const BasicTemp = [
   { label: "And operator", value: and },
-  { label: "Or operator", value: or },
+  { label: "Between", value: between },
+  { label: "Command", value: command },
+  { label: "Drops Below", value: dropsBelow },
+  { label: "Drops To Or Below", value: dropsToOrBelow },
   { label: "If Else", value: ifElse },
-  { label: "If Not", value: ifNot },
   { label: "If Greater Than", value: ifGreaterThan },
   { label: "If Greater Than or Equals", value: ifGreaterThanOrEquals },
   { label: "If less Than", value: lessThan },
   { label: "If less Than or Equals", value: lessThanOrEquals },
-  { label: "Between", value: between },
+  { label: "If Not", value: ifNot },
+  { label: "Or operator", value: or },
+  { label: "Rises Above", value: risesAbove },
+  { label: "Rises To Or Above", value: risesToOrAbove },
+  { label: "Set Location Mode", value: setLocationMode },
 ];
 
 const AdvancedTemp = [
   { label: "Nested If Condition", value: nestedIf },
+  { label: "Morning Schedule", value: morningSchedule },
   { label: "Sleep", value: sleep },
-  { label: "", value: "" },
 ];
 
 
