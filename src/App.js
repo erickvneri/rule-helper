1c98cf1e7986c1b0b036f64dfb8ab3adb40bd3ab
erickvneri <erickv@bluetrailsoft.com>
2020-10-30 14:53:41 -0600

    Navigation component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 // Components
+import Navigation from "./components/Navigation";
 import RuleExamples from "./components/RuleExamples";
 import BasicTemplates from "./components/BasicTemplates";
 import AdvancedTemplates from "./components/AdvancedTemplates";
@@ -10,7 +11,8 @@ function App() {
   return (
     <main>
       <Switch>
-        <Route path='/' component={RuleExamples} exact/>
+        <Route path='/' component={Navigation} exact/>
+        <Route path='/rule-examples' component={RuleExamples} exact/>
         <Route path='/basic-templates' component={BasicTemplates} exact/>
         <Route path='/advanced-templates' component={AdvancedTemplates} exact/>
       </Switch>
