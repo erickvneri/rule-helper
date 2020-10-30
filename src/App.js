20c3a3d7db83025ab560af8a18458c57c6661f0f
erickvneri <erickv@bluetrailsoft.com>
2020-10-30 00:21:36 -0600

    components routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 // Components
-import RuleTemplates from "./components/RuleTemplates";
-import { Switch, Route } from "react-router-dom";
-
+import RuleExamples from "./components/RuleExamples";
+import BasicTemplates from "./components/BasicTemplates";
+import AdvancedTemplates from "./components/AdvancedTemplates";
 
 function App() {
   return (
     <main>
       <Switch>
-        <Route path='/' component={RuleTemplates} exact/>
+        <Route path='/' component={RuleExamples} exact/>
+        <Route path='/basic-templates' component={BasicTemplates} exact/>
+        <Route path='/advanced-templates' component={AdvancedTemplates} exact/>
       </Switch>
     </main>
   );
