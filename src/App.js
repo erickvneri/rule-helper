430ebb320ff2fb415f819a64eb1fa93006f68e95
erickvneri <erickv@bluetrailsoft.com>
2020-10-30 18:39:33 -0600

    Navigation -> Home component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 // Components
-import Navigation from "./components/Navigation";
+import Home from "./components/Home";
 import RuleExamples from "./components/RuleExamples";
 import BasicTemplates from "./components/BasicTemplates";
 import AdvancedTemplates from "./components/AdvancedTemplates";
@@ -11,7 +11,7 @@ function App() {
   return (
     <main>
       <Switch>
-        <Route path='/' component={Navigation} exact/>
+        <Route path='/' component={Home} exact/>
         <Route path='/rule-examples' component={RuleExamples} exact/>
         <Route path='/basic-templates' component={BasicTemplates} exact/>
         <Route path='/advanced-templates' component={AdvancedTemplates} exact/>
---
+++ src/components/Home.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ButtonRow from "react-bootstrap/ButtonGroup";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../App.scss";
+
+
+function Home() {
+  return (
+    <div className="container">
+      <h1 className="title">Rule Helper</h1>
+
+      <p>   The <em>Rule Helper</em> is an application intended to improve the understanding of the Rules API. Also, it pretends to
+      enhance the experience of developers and hobbyists while building Home Automation solutions.</p>
+      <br></br>
+      <p>
+        <strong>Some of the perks about building Rules are:</strong>
+      </p>
+        <li>Rules are based on the JSON format which is easy to understand.</li>
+        <li>Control flow statements and context managers are very intuitive.</li>
+        <li>Complexity of your Rule will rely on your creativity.</li>
+        <li>The flexibility to build different solutions to the same problem, etc.</li>
+      <br></br>
+      <p>
+        <strong>About the Rule Helper:</strong>
+        The <em>Rule Helper</em> is devided in 3 components.
+      </p>
+      <li>The <strong>Rule Examples</strong> component offers a series of Rules that cover multiple use cases with the current Rules API features.</li>
+      <li>The <strong>Basic Templates</strong> component provides an introduction to different conditionals to create Rules.</li>
+      <li>The <strong>Advanced Templates</strong> component combines elements presented by Basic Templates and illustrates the
+      flexibility of nested conditionals and concatenated actions.</li>
+      <br></br>
+
+      <ButtonRow style={{width: "150%"}}>
+        <Link to="/rule-examples">
+          <Button className="button" style={{width: "20%"}}>Rule Examples</Button>
+        </Link>
+        <Link to="/basic-templates">
+          <Button className="button" style={{width: "20%"}}>Basic Templates</Button>
+        </Link>
+        <Link to="/advanced-templates">
+          <Button className="button" style={{width: "20%"}}>Advanced Templates</Button>
+        </Link>
+      </ButtonRow>
+    </div>
+  )
+};
+
+
+export default Home;
